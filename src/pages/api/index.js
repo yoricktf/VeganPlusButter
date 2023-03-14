d8f0907c6aa4692ae57ba314e040b6bb78510c4a
yoricktf <yorick.tenfeld@gmail.com>
2023-03-14 10:53:26 +0100

    added the signup and login with google

--- src/pages/api/index.js
+++ src/pages/api/index.js
@@ -6,7 +6,6 @@ export default async function handler(req, res) {
   dbConnect()
   if (req.method === "GET") {
     const posts = await Post.find()
-    console.log(posts)
     res.status(200).json(posts)
   }
 }
