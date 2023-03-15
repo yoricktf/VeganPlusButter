5db22ed63cc197ac4a6cfb9791accf885786f707
yoricktf <yorick.tenfeld@gmail.com>
2023-03-15 10:29:22 +0100

    looping through tags and ingredients to lower case them all for search

--- src/pages/search.js
+++ src/pages/search.js
@@ -7,21 +7,26 @@ const Search = ({ posts }) => {
   const handleSearch = (event) => {
     setSearchQuery(event.target.value.toLowerCase())
   }
-
-  return (
-    <>
-      <h1>Search</h1>
-      <input type="text" onChange={handleSearch} placeholder="cookies, healthy, butter" />
-      <p className="detail">Search through recipe titles, tags or ingredients</p>
-      {posts.filter(post => {
-        return post.title.toLowerCase().includes(searchQuery) || post.tags.toLowerCase().includes(searchQuery) || post.ingredients.toLowerCase().includes(searchQuery)
-      }).map(foundPost => {
-        return (
-          <LargeCard key={foundPost._id} recipeInfo={foundPost} />
-        )
-      })}
-    </>
-  )
+  console.log(!!posts)
+  if (!!posts) {
+    return (
+      <>
+        <h1>Search</h1>
+        <input type="text" onChange={handleSearch} placeholder="cookies, healthy, butter" />
+        <p className="detail">Search through recipe titles, tags or ingredients</p>
+        {posts.filter(post => {
+          return (post.title.toLowerCase()).includes(searchQuery)
+            || (post.tags.map(tag => tag.toLowerCase())).includes(searchQuery)
+            || (post.ingredients.map(ingredient => ingredient.toLowerCase())).includes(searchQuery)
+        }).map(foundPost => {
+          return (
+            <LargeCard key={foundPost._id} recipeInfo={foundPost} />
+          )
+        })}
+      </>
+    )
+  }
+  return <h1>Loading</h1>
 }
 
 export default Search
