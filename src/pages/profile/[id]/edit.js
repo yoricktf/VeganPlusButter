a1946c0e4a316f309b08e21af64a5c04489a0d82
yoricktf <yorick.tenfeld@gmail.com>
2023-03-17 16:05:55 +0100

    profilepage styling

--- src/pages/profile/[id]/edit.js
+++ src/pages/profile/[id]/edit.js
@@ -38,20 +38,24 @@ const EditProfile = (
 
   if (!!specificUser) {
     return (
-      <>
-        <Image className='largeProfile' src={specificUser.image} width={96} height={96} alt={`${specificUser.name}'s profile picture`} />
-        <h1>{specificUser.name}&apos;s Profile</h1>
+      <section className='profileForm'>
+        <div className='profileData'>
+          <Image id='largeProfile' src={specificUser.image} width={96} height={96} alt={`${specificUser.name}'s profile picture`} />
+          <div>
+            <h1>{specificUser.name}&apos;s Profile</h1>
+            <p>email: {specificUser.email}</p>
 
-        <p>email: {specificUser.email}</p>
+          </div>
+        </div>
 
         <form className='userForm' onSubmit={handleEditUserDetails}>
-          <label htmlFor="name"></label>
+          <label htmlFor="name">Name:</label>
           <input type="text" id='name' name='name' onChange={(e) => setSpecificUser({ ...specificUser, name: e.target.value })} value={specificUser.name} />
           <label htmlFor="bio">Bio:</label>
-          <textarea name="bio" id="bio" cols="30" rows="10" onChange={(e) => setSpecificUser({ ...specificUser, bio: e.target.value })} value={specificUser.bio}></textarea>
+          <textarea placeholder='Enter your Bio here' name="bio" id="bio" cols="30" rows="10" onChange={(e) => setSpecificUser({ ...specificUser, bio: e.target.value })} value={specificUser.bio}></textarea>
           <button>Edit Your Profile</button>
         </form>
-      </>
+      </section>
     )
   }
   return <h1>Loading</h1>
