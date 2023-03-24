71d4417adea55922e253a3d70c452ab491a32eae
yoricktf <yorick.tenfeld@gmail.com>
2023-03-24 14:32:29 +0100

    showing the favorites and the comments on the profile page

--- src/pages/api/user/[id].js
+++ src/pages/api/user/[id].js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
   const { id } = req.query
 
   if (req.method === "GET") {
-    const user = await User.findById(id)
+    const user = await User.findById(id).populate('favorites')
     res.status(200).json(user)
   }
 
