8ee6b4669ed848d2d9d4314af46529279ef646aa
yoricktf <yorick.tenfeld@gmail.com>
2023-04-03 15:36:13 +0200

    dropdown implemented using materialUI

--- components/Login-btn.js
+++ components/Login-btn.js
@@ -2,11 +2,24 @@ import { useSession, signIn, signOut } from "next-auth/react"
 import Image from "next/image"
 import Link from 'next/link'
 import { useEffect, useState } from "react"
-import { useRouter } from 'next/router'
+import * as React from 'react';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import IconButton from '@mui/material/IconButton';
+import Avatar from '@mui/material/Avatar';
+
 
 export default function LoginComponent() {
   const [loggedInUser, setLoggedInUser] = useState()
   const { data: session } = useSession()
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
@@ -34,23 +47,34 @@ export default function LoginComponent() {
   if (session && loggedInUser) {
     return (
       <div className="loginOptions dropdown">
-        {/* <button onClick={() => signOut()}>Sign out</button> */}
-        {/* <Link href={`/profile/${loggedInUser._id}`}> */}
-        <Image
-          onClick={myFunction}
-
-          className="profile"
-          src={session.user.image}
-          height={30}
-          width={30}
-          alt={`profile picture for ${session.user.name}`} />
-        <div id="myDropdown" className="dropdown-content">
-          <Link href={`/profile/${loggedInUser._id}`}>Profile</Link>
-          <Link href={`/about}`}>About</Link>
-          <p onClick={() => signOut()}>Sign Out</p>
-        </div>
-
-        {/* </Link> */}
+        <IconButton
+          onClick={handleClick}
+          size="small"
+          sx={{ ml: 2 }}
+          aria-controls={open ? 'account-menu' : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? 'true' : undefined}
+        >
+          <Avatar sx={{ width: 32, height: 32 }}> <Image
+            className="profile"
+            src={session.user.image}
+            height={30}
+            width={30}
+            alt={`profile picture for ${session.user.name}`} /></Avatar>
+        </IconButton>
+        <Menu
+          id="basic-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          MenuListProps={{
+            'aria-labelledby': 'basic-button',
+          }}
+        >
+          <Link href={`/profile/${loggedInUser._id}`}><MenuItem onClick={handleClose}>Profile</MenuItem></Link>
+          <Link href={`/about}`}><MenuItem onClick={handleClose}>About</MenuItem></Link>
+          <MenuItem onClick={() => signOut()}>Logout</MenuItem>
+        </Menu>
       </div>
     )
   }
