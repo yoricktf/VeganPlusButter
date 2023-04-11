6415288121dc193d23ef3112f4f6ebea822209de
yoricktf <yorick.tenfeld@gmail.com>
2023-04-11 15:55:47 +0200

    added about page and getting all admin from the DB and displaying bio and image and linking to their profile pages

--- src/pages/api/users.js
+++ src/pages/api/users.js
@@ -4,10 +4,19 @@ import User from "../../../db/models/User"
 export default async function handler(req, res) {
   await dbConnect()
 
-  const userObject = JSON.parse(req.body)
 
   if (req.method === "POST") {
+    const userObject = JSON.parse(req.body)
     const user = await User.find({ email: userObject.email })
     res.status(200).json(user)
+  }
+
+  if (req.method === "GET") {
+    let admins = []
+    const users = await User.find()
+    for (let user of users) {
+      user.admin === true ? admins.push(user) : null
+    }
+    res.status(200).json(admins)
   }
 }
