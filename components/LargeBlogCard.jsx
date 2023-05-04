444fbcb0782a68269965fd945becf334a0028096
yoricktf <yorick.tenfeld@gmail.com>
2023-05-04 13:34:09 +0200

    styled the largeblog cards to slideinto view on scroll

--- components/LargeBlogCard.jsx
+++ components/LargeBlogCard.jsx
@@ -2,10 +2,21 @@ import Link from 'next/link'
 import React from 'react'
 
 const LargeBlogCard = ({ blogPost }) => {
-
   const { title, description, date } = blogPost
-
   const shortenedDescription = description.slice(0, 150)
+
+  const observer = new IntersectionObserver((blogPosts) => {
+    blogPosts.forEach((blogPost) => {
+      if (blogPost.isIntersecting) {
+        blogPost.target.classList.add('show');
+      } else {
+        blogPost.target.classList.remove('show');
+      }
+    });
+  });
+
+  const hiddenBlogPosts = document.querySelectorAll('.largeBlogCard');
+  hiddenBlogPosts.forEach((blogpost) => observer.observe(blogpost));
 
   return (
     <article className='largeBlogCard'>
