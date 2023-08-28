48bce716d57f0ac296a33e2b35f94e6804770da4
yoricktf <yorick.tenfeld@gmail.com>
2023-08-28 12:55:13 +0200

    try catch in the backend and logging the problem when /api dos not respond

--- src/pages/api/index.js
+++ src/pages/api/index.js
@@ -1,11 +1,15 @@
-import dbConnect from "../../../db/connect"
-import Post from "../../../db/models/Post"
-
+import dbConnect from '../../../db/connect';
+import Post from '../../../db/models/Post';
 
 export default async function handler(req, res) {
-  await dbConnect()
-  if (req.method === "GET") {
-    const posts = await Post.find().populate('author')
-    res.status(200).json(posts)
+  await dbConnect();
+  if (req.method === 'GET') {
+    try {
+      const posts = await Post.find().populate('author');
+      res.status(200).json(posts);
+    } catch (error) {
+      console.log('ERROR MESSAGES=========', error);
+      res.status(500).json({ error: error.message });
+    }
   }
 }
