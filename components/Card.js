049668378b6a2c5d4e2249a0c38be55370820aaf
yoricktf <yorick.tenfeld@gmail.com>
2023-08-31 13:21:54 +0200

    changed string length to fit card better

--- components/Card.js
+++ components/Card.js
@@ -1,31 +1,31 @@
-import Image from "next/image"
-import Link from "next/link"
-import Tag from "./Tag"
+import Image from 'next/image';
+import Link from 'next/link';
+import Tag from './Tag';
 const Card = ({ title, image, tags, postId }) => {
-
-  const shortenedTitle = title.length > 27 ? title.slice(0, 27) + '...' : title
+  const shortenedTitle = title.length > 26 ? title.slice(0, 26) + '...' : title;
 
   return (
     <Link href={`recipe/${postId}`}>
-      <article className="recipeCard">
-        <div className="layer">
+      <article className='recipeCard'>
+        <div className='layer'>
           <Image
-            className="cardImage"
+            className='cardImage'
             src={image}
-            alt="recipe image"
+            alt='recipe image'
             fill
-            sizes="100%"
+            sizes='100%'
             style={{ objectFit: 'cover' }}
-
           />
-          <div className="cardTags">
-            {tags.map(tag => <Tag key={tag} tag={tag} />)}
+          <div className='cardTags'>
+            {tags.map((tag) => (
+              <Tag key={tag} tag={tag} />
+            ))}
           </div>
         </div>
-        <h2 className="cardTitle">{shortenedTitle}</h2>
+        <h2 className='cardTitle'>{shortenedTitle}</h2>
       </article>
     </Link>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
