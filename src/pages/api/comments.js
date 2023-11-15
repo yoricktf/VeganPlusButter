f61ca1f63629d662e808b3d846ff7b86486bf2b8
yoricktf <yorick.tenfeld@gmail.com>
2023-11-15 16:35:33 +0100

    fethcing the comments using SWR and filtereing on the backend

--- src/pages/api/comments.js
+++ src/pages/api/comments.js
@@ -1,25 +1,45 @@
-import dbConnect from "../../../db/connect"
-import Comment from "../../../db/models/Comment"
+import dbConnect from '../../../db/connect';
+import Comment from '../../../db/models/Comment';
 
 export default async function handler(req, res) {
-  await dbConnect()
+  await dbConnect();
 
-  if (req.method === "GET") {
-    try {
-      const allComments = await Comment.find().populate('author')
-      res.status(200).json(allComments)
-    } catch (error) {
-      res.status(500).json({ error: error.message })
+  // if (req.method === 'GET') {
+  //   try {
+  //     const allComments = await Comment.find().populate('author');
+  //     res.status(200).json(allComments);
+  //   } catch (error) {
+  //     res.status(500).json({ error: error.message });
+  //   }
+  // }
+  if (req.method === 'GET') {
+    if (req.query.type === 'User Comments') {
+      try {
+        const filteredComments = await Comment.find({
+          author: req.query.authorId,
+        }).populate('author');
+        console.log('----------------------', filteredComments);
+        res.status(200).json(filteredComments);
+      } catch (error) {
+        res.status(500).json({ error: error.message });
+      }
     }
+
+    // try {
+    //   const allComments = await Comment.find().populate('author');
+    //   res.status(200).json(allComments);
+    // } catch (error) {
+    //   res.status(500).json({ error: error.message });
+    // }
   }
 
-  if (req.method === "POST") {
+  if (req.method === 'POST') {
     try {
-      const commentObject = await JSON.parse(req.body)
-      const submittedComment = await Comment.create(commentObject)
-      res.status(200).json(submittedComment)
+      const commentObject = await JSON.parse(req.body);
+      const submittedComment = await Comment.create(commentObject);
+      res.status(200).json(submittedComment);
     } catch (error) {
-      res.status(500).json({ error: error.message })
+      res.status(500).json({ error: error.message });
     }
   }
 }
--- src/pages/profile/[id]/index.js
+++ src/pages/profile/[id]/index.js
@@ -1,89 +1,129 @@
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import Image from 'next/image';
-import { useSession } from "next-auth/react"
+import { useSession } from 'next-auth/react';
 import Link from 'next/link';
 import NotAuthorized from '../../../../components/NotAuthorized';
 import LargeCard from '../../../../components/LargeCard';
 import Comment from '../../../../components/Comment';
+import Loading from '../../../../components/Loading';
+import useSWR from 'swr';
 
-const Index = (
-  { comments }
+const Index = ({ comments }) =>
   // { handleFetchSpecificUser }
-) => {
-  const { data: session, status } = useSession()
-  const router = useRouter()
-  const { id } = router.query;
-  const [specificUser, setSpecificUser] = useState()
-  const [filteredComments, setFilteredComments] = useState([])
+  {
+    const { data: session, status } = useSession();
+    const router = useRouter();
+    const { id } = router.query;
+    // const [specificUser, setSpecificUser] = useState();
+    // const [filteredComments, setFilteredComments] = useState([]);
 
-  // useEffect(() => {
-  // setSpecificUser(handleFetchSpecificUser(id))
-  // }, [id])
-  const fetchUsersComments = async () => {
-    if (id) {
-      const response = await fetch('/api/comments')
-      const comments = await response.json()
-      const filteredComments = comments.filter(comment => comment.author._id === id)
-      const sortedUsersComments = filteredComments.sort(function (a, b) {
-        return (a.createdAt > b.createdAt) ? -1 : ((a.createdAt < b.createdAt) ? 1 : 0);
-      });
-      setFilteredComments(sortedUsersComments)
+    const {
+      data: filteredComments,
+      error: errorComments,
+      isLoading: loadingComments,
+    } = useSWR(`/api/comments?type=User Comments&authorId=${id}`);
+
+    const {
+      data: specificUser,
+      error: errorSpecificUser,
+      isLoading: loadingSpecificUser,
+    } = useSWR(`/api/user/${id}`);
+
+    if (errorComments || errorSpecificUser) {
+      return <div>failed to load</div>;
     }
-  }
 
-  const fetchSpecficUser = async () => {
-    if (id) {
-      const response = await fetch(`/api/user/${id}`)
-      const user = await response.json()
-      setSpecificUser(user)
+    if (loadingComments || loadingSpecificUser) {
+      return <Loading />;
     }
-  }
 
-  useEffect(() => {
-    fetchSpecficUser()
-    fetchUsersComments()
-  }, [id])
+    // useEffect(() => {
+    // setSpecificUser(handleFetchSpecificUser(id))
+    // }, [id])
+    const fetchUsersComments = async () => {
+      if (id) {
+        const response = await fetch('/api/comments');
+        const comments = await response.json();
+        const filteredComments = comments.filter(
+          (comment) => comment.author._id === id
+        );
+        const sortedUsersComments = filteredComments.sort(function (a, b) {
+          return a.createdAt > b.createdAt
+            ? -1
+            : a.createdAt < b.createdAt
+            ? 1
+            : 0;
+        });
+        setFilteredComments(sortedUsersComments);
+      }
+    };
 
+    const fetchSpecficUser = async () => {
+      if (id) {
+        const response = await fetch(`/api/user/${id}`);
+        const user = await response.json();
+        setSpecificUser(user);
+      }
+    };
 
-  if (status === 'authenticated') {
-    return (
-      <section className='profilePage bodySection'>
-        <div className='profileData'>
-          {!!specificUser ?
-            <Image className='largeProfile' src={specificUser?.image} width={96} height={96} alt={`${specificUser?.name}'s profile picture`} />
-            :
-            null
-          }
-          <div>
-            <h1>{specificUser?.name}&apos;s Profile</h1>
-            <p>Bio: {specificUser?.bio}</p>
+    // useEffect(() => {
+    //   fetchSpecficUser();
+    //   fetchUsersComments();
+    // }, [id]);
+
+    if (status === 'authenticated') {
+      return (
+        <section className='profilePage bodySection'>
+          <div className='profileData'>
+            {!!specificUser ? (
+              <Image
+                className='largeProfile'
+                src={specificUser?.image}
+                width={96}
+                height={96}
+                alt={`${specificUser?.name}'s profile picture`}
+              />
+            ) : null}
+            <div>
+              <h1>{specificUser?.name}&apos;s Profile</h1>
+              <p>Bio: {specificUser?.bio}</p>
+            </div>
           </div>
-        </div>
-        {specificUser?.email === session.user.email ? <Link className='button' href={`/profile/${specificUser._id}/edit`}>Edit your Profile</Link> : ''}
-        <section className='usersFavorites'>
-          <h2>{specificUser?.name}&apos;s Favorites</h2>
-          {specificUser?.favorites?.map((favoritedRecipe, index) => {
-            return (
-              <LargeCard key={index} recipeInfo={favoritedRecipe} comments={comments} />
-            )
-          })}
+          {specificUser?.email === session.user.email ? (
+            <Link className='button' href={`/profile/${specificUser._id}/edit`}>
+              Edit your Profile
+            </Link>
+          ) : (
+            ''
+          )}
+          <section className='usersFavorites'>
+            <h2>{specificUser?.name}&apos;s Favorites</h2>
+            {specificUser?.favorites?.map((favoritedRecipe, index) => {
+              return (
+                <LargeCard
+                  key={index}
+                  recipeInfo={favoritedRecipe}
+                  comments={comments}
+                />
+              );
+            })}
+          </section>
+          <section className='comments'>
+            <h2>Comments</h2>
+            {filteredComments.map((comment, index) => (
+              <Comment
+                key={index}
+                userComment={comment}
+                fetchUsersComments={fetchUsersComments}
+              />
+            ))}
+          </section>
         </section>
-        <section className='comments'>
-          <h2>Comments</h2>
-          {filteredComments.map((comment, index) => <Comment key={index} userComment={comment} fetchUsersComments={fetchUsersComments} />)}
-        </section>
-      </section>
-    )
-  } else {
-    return (
-      <NotAuthorized />
-    )
-  }
+      );
+    } else {
+      return <NotAuthorized />;
+    }
+  };
 
-  return (
-    <h1>Loading...</h1>
-  )
-}
-
-export default Index
+export default Index;
