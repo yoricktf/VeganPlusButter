e007af308bd466100eb63498fd58b73175210e90
yoricktf <yorick.tenfeld@gmail.com>
2024-02-08 11:39:29 +0100

    vscode reformatted the code

--- src/pages/api/users.js
+++ src/pages/api/users.js
@@ -1,22 +1,21 @@
-import dbConnect from "../../../db/connect"
-import User from "../../../db/models/User"
+import dbConnect from '../../../db/connect';
+import User from '../../../db/models/User';
 
 export default async function handler(req, res) {
-  await dbConnect()
+  await dbConnect();
 
-
-  if (req.method === "POST") {
-    const userObject = JSON.parse(req.body)
-    const user = await User.find({ email: userObject.email })
-    res.status(200).json(user)
+  if (req.method === 'POST') {
+    const userObject = JSON.parse(req.body);
+    const user = await User.find({ email: userObject.email });
+    res.status(200).json(user);
   }
 
-  if (req.method === "GET") {
-    let admins = []
-    const users = await User.find()
+  if (req.method === 'GET') {
+    let admins = [];
+    const users = await User.find();
     for (let user of users) {
-      user.admin === true ? admins.push(user) : null
+      user.admin === true ? admins.push(user) : null;
     }
-    res.status(200).json(admins)
+    res.status(200).json(admins);
   }
 }
